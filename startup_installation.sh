87f650580626c907cbc2426895676051eb24fd1a
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-01-21 10:56:42 -0500

    More development stuff added

--- startup_installation.sh
+++ startup_installation.sh
@@ -2,7 +2,7 @@
 
 cd
 mkdir -p Downloads Documents Pictures AzureDevOps GitHub/Youngermaster GitLab/Youngermaster
-sudo pacman -Syu iwd git vim neovim xorg xorg-xinit dmenu lxsession ranger lsd bat libx11 base-devel firefox flameshot neofetch alacritty feh networkmanager brightnessctl htop starship alsa-lib alsa-plugins alsa-utils nautilus dunst
+sudo pacman -Syu iwd git vim neovim xorg xorg-xinit dmenu lxsession ranger lsd bat libx11 base-devel firefox flameshot neofetch alacritty feh networkmanager brightnessctl htop starship alsa-lib alsa-plugins alsa-utils nautilus dunst zathura zathura-pdf-mupdf scrcpy
 
 ## Graphic Drivers
 sudo pacman -Syu nvidia nvidia-utils nvidia-settings
@@ -14,10 +14,28 @@ sudo pacman -Syu neofetch bspwm sxhkd nitrogen picom rofi
 paru -S lxappearance nordic-darker-theme lightdm betterlockscreen lightdm-webkit2-greeter wpgtk-git spicetify-cli-git
 
 ## Virtualbox stuff
-sudo pacman -Syu virtualbox virtualbox-ext-vnc virtualbox-guest-iso virtualbox-guest-utils virtualbox-host-dkms 
+sudo pacman -Syu virtualbox virtualbox-ext-vnc virtualbox-guest-iso virtualbox-guest-utils virtualbox-host-modules-arch
+### Reboot the system an run
+#### sudo modprove vboxdrv
 
 ## More development stuff
-sudo pacman -Syu nodejs npm
+### Remember to reboot the system after the installation of the keyring stuff
+sudo pacman -Syu nodejs npm texlive-core texlive-latexextra rubber
+xdg-mime default zathura.desktop application/pdf
+paru -S visual-studio-code-bin
+sudo pacman -S gnome-keyring libsecret libgnome-keyring seahorse
+echo "# see https://unix.stackexchange.com/a/295652/332452                                                                                                                                        ─╯
+source /etc/X11/xinit/xinitrc.d/50-systemd-user.sh
+
+# see https://wiki.archlinux.org/title/GNOME/Keyring\#xinitrc
+eval $(/usr/bin/gnome-keyring-daemon --start)
+export SSH_AUTH_SOCK
+
+# see https://github.com/NixOS/nixpkgs/issues/14966\#issuecomment-520083836
+mkdir -p "$HOME"/.local/share/keyrings" >> ~/.xinitrc
+
+## OBS
+sudo pacman -Syu ffmpeg obs-studio vlc
 
 ## Ethernet stuff
 sudo ip link set enp2s0 up
