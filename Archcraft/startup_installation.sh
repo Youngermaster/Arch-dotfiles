66859272655b4eb01e4dfb67466d371168d18259
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2023-03-07 12:36:27 -0500

    Hyprland dependencies added to the project

--- Archcraft/startup_installation.sh
+++ Archcraft/startup_installation.sh
@@ -101,6 +101,7 @@ flatpak install flathub md.obsidian.Obsidian -y
 yay -S mongosh-bin mongodb-bin mongodb-tools-bin mongodb-compass
 
 # Hyperconfig
+yay -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info
 yay -S hyprland-nvidia-git touchegg
 sudo systemctl enable touchegg.service && sudo systemctl start touchegg.service
 yay -S polkit-gnome ffmpeg neovim viewnior       \
